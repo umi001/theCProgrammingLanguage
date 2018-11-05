64f009eaf6a40dde7328147d4339831ef1e9d6e6
umi <mubaenn001@gmail.com>
2018-11-06 02:41:40 +0530

    ex1.9.c

--- Chapter1/1-9.c
+++ Chapter1/1-9.c
@@ -1,3 +1,17 @@
 #include <stdio.h>
 void main(){
+	int c,nspaces;
+	nspaces=0;
+
+	while((c=getchar())!=EOF){
+		putchar(c);
+		while(c==' ' && c!=EOF){
+			c=getchar();
+			if(c!=' '){
+				putchar(c);
+			}
+		}	
+	}
+}
+
 
