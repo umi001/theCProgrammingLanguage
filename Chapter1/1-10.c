8dff3409d760f1902148025298854033a76fdab2
umi <mubaenn001@gmail.com>
2018-11-06 03:00:16 +0530

    ex 1-10.c

---
+++ Chapter1/1-10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void main(){
+	int c;
+
+	while((c=getchar())!=EOF){
+		if(c=='\t'){
+			putchar('\\');
+			putchar('t');
+		}
+		if(c=='\b'){
+			putchar('\\');
+			putchar('b');
+			}
+		if(c=='\\'){
+			putchar('\\');
+			putchar('\\');
+		}
+		if(c != '\t' && c != '\b' & c != '\\')
+            		putchar(c);
+    	}
+
+}
+	
+
+
