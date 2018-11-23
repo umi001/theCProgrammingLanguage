aebd549db965ea5a98e3a6bf0fd8d9235bda1c35
umi <mubaenn001@gmail.com>
2018-11-23 23:44:09 +0530

    Till 1.17

---
+++ Chapter1/1-17.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#define MAXLEN 1000
+#define CUTOFFLEN 80
+
+int getLine(char l[]);
+void copyLine(char l[],char lon[]);
+
+int main(){
+	int len;
+	char line[MAXLEN];
+
+	while((len=getLine(line))>0){
+		if(len>CUTOFFLEN){
+			printf("%s\n",line);
+		}
+	}
+
+	return 0;
+}
+
+int getLine(char line[]){
+	int c,l;
+	l=0;
+	while(((c=getchar())!=EOF)&&(c!='\n')&&(l<MAXLEN)){
+		line[l]=c;
+		++l;
+	}
+	if(c=='\n'){
+		line[l]=c;
+		++l;
+	}
+	line[l]='\0';
+	return l;
+}
+
+
---
+++ Chapter1/longestLine.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#define MAXLEN 1000
+
+int getLine(char l[]);
+void copyLine(char l[],char lon[]);
+
+int main(){
+	int len,max,i;
+	char line[MAXLEN],longline[MAXLEN];
+	max=0;
+
+	while((len=getLine(line))>0){
+		if(len>max){
+			max=len;
+			copyLine(line,longline);
+		}
+	}
+
+	if(max>0){
+		printf("%s",longline);
+	}
+	return 0;
+}
+
+int getLine(char line[]){
+	int c,l;
+	l=0;
+	while(((c=getchar())!=EOF)&&(c!='\n')&&(l<MAXLEN)){
+		line[l]=c;
+		++l;
+	}
+	if(c=='\n'){
+		line[l]=c;
+		++l;
+	}
+	line[l]='\0';
+	return l;
+}
+
+void copyLine(char line[],char longline[]){
+	int i;
+	i=0;
+	while((longline[i]=line[i])&&(line[i]!='\0')){
+		++i;
+	}
+}
