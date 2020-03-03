cb47ca48699fb032a4067b8b6ac83f9991637fbf
umi001 <mubaenn001@gmail.com>
2020-03-03 21:53:37 +0530

    Added Ex 1-8

---
+++ Chapter1/1-8.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main(){
+	int c;
+	int line_count, blank_count, tab_count;
+	line_count = 0;
+	blank_count =0;
+	tab_count =0;
+
+	while((c=getchar()) != EOF){
+		if(c == ' '){
+			++blank_count;
+		}
+		else if(c == '\n'){
+			++line_count;
+		}
+		else if(c == '\t'){
+			++tab_count;
+		}
+	}
+	printf("No of blanks: %d\nNo of tabs: %d\nNo of lines: %d\n",blank_count,tab_count,line_count);
+}
+		
+
